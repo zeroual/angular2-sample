5be7c7f61843fc9c4ca92519b9365b8f1b61c833
zeroual <abid1991@hotmail.fr>
2017-11-02 22:35:53 +0100

    create a service that return promised heroes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,30 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Hero} from './hero';
+import {HeroService} from './hero.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+
+  heroes: Hero[];
+
+  constructor(private  heroService: HeroService) {
+  }
 
   title = 'Tour of heroes';
-  heroes = HEROES;
   selectedHero: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-}
+  ngOnInit(): void {
+    this.heroService.getHeroes().then(values => this.heroes = values);
+  }
 
-const HEROES: Hero[] = [
-  {id: 11, name: 'Mr. Nice'},
-  {id: 12, name: 'Narco'},
-  {id: 13, name: 'Bombasto'},
-  {id: 14, name: 'Celeritas'},
-  {id: 15, name: 'Magneta'},
-  {id: 16, name: 'RubberMan'},
-  {id: 17, name: 'Dynama'},
-  {id: 18, name: 'Dr IQ'},
-  {id: 19, name: 'Magma'},
-  {id: 20, name: 'Tornado'}
-];
+}
