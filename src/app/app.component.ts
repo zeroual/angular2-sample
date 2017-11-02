154638d19c4c0c70d935798aa591a249e29b2bac
zeroual <abid1991@hotmail.fr>
2017-11-02 22:09:54 +0100

    create multiple components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Hero} from './hero';
 
 @Component({
   selector: 'app-root',
@@ -16,12 +17,6 @@ export class AppComponent {
     this.selectedHero = hero;
   }
 
-}
-
-
-export class Hero {
-  id: number;
-  name: string;
 }
 
 const HEROES: Hero[] = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule} from '@angular/forms';
+import {HeroDetailComponent} from './hero-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent,
   ],
   imports: [
     BrowserModule,
